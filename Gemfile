ed765c426e8d0303c4439d2c76a8488e06a1d34a
Camila Ferreira <camilaferreira68@hotmail.com>
2024-04-07 22:00:19 -0300

    feat: adicionar gem authentication zero

--- Gemfile
+++ Gemfile
@@ -66,3 +66,7 @@ group :test do
   gem "selenium-webdriver"
   gem "webdrivers"
 end
+
+gem "authentication-zero", "~> 3.0"
+# Use Active Model has_secure_password [https://guides.rubyonrails.org/active_model_basics.html#securepassword]
+gem "bcrypt", "~> 3.1.7"
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_08_005105) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_08_005447) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -48,7 +48,26 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_08_005105) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "sessions", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "user_agent"
+    t.string "ip_address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_sessions_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", null: false
+    t.string "password_digest", null: false
+    t.boolean "verified", default: false, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+  end
+
   add_foreign_key "animais", "especies", column: "especie_id"
   add_foreign_key "animais", "recintos"
   add_foreign_key "historicos_animais", "animais"
+  add_foreign_key "sessions", "users"
 end
