846765775882e7a03ecba4ece65028a2f45eff46
Camila Ferreira <camilaferreira68@hotmail.com>
2024-04-14 20:17:58 -0300

    chore: adicionar i18n

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,6 @@ module ProntuarioVeterinario
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.i18n.default_locale = :"pt-BR"
   end
 end
