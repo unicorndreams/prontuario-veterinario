d4840c0e82fcd9e50dc20c87e62117281d30a942
Camila Ferreira <camilaferreira68@hotmail.com>
2024-04-14 20:50:10 -0300

    feat: criar endpoint de espécies

---
+++ app/controllers/especies_controller.rb
@@ -0,0 +1,56 @@
+class EspeciesController < ApplicationController
+  before_action :set_especie, only: [:edit, :update, :destroy]
+
+  def index
+    @especies = current_user.especies.order(:nome)
+  end
+
+  def new
+    @especie = current_user.especies.new
+  end
+
+  def edit
+  end
+
+  def update
+    respond_to do |format|
+      if @especie.update(especie_params)
+        redirect_to especies_path
+      else
+        format.turbo_stream { render turbo_stream: turbo_stream.replace("remote_modal", partial: "especies/modal", locals: { especie: @especie }) }
+      end
+    end
+  end
+
+  def create
+    @especie = current_user.especies.new(especie_params)
+
+    respond_to do |format|
+      if @especie.save
+        redirect_to especies_path
+      else
+        format.turbo_stream { render turbo_stream: turbo_stream.replace("remote_modal", partial: "especies/modal", locals: { especie: @especie }) }
+      end
+    end
+  end
+
+  def destroy
+    respond_to do |format|
+      if @especie.destroy
+        format.turbo_stream { render turbo_stream: turbo_stream.remove("especie-#{@especie.id}") }
+      else
+        format.turbo_stream { render turbo_stream: turbo_stream.replace("error", partial: "shared/error", locals: { error: @especie.errors.full_messages.join(", ") }) }
+      end
+    end
+  end
+
+  private
+
+  def set_especie
+    @especie = current_user.especies.find(params[:id])
+  end
+
+  def especie_params
+    params.require(:especie).permit(:nome)
+  end
+end
--- app/models/especie.rb
+++ app/models/especie.rb
@@ -1,5 +1,5 @@
 class Especie < ApplicationRecord
-  has_many :animais
+  has_many :animais, dependent: :restrict_with_error
   belongs_to :user
 
   validates :nome, presence: true
