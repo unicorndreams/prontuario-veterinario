30ce8e055e14725d7354950112303a05507fbbe2
Camila Ferreira <camilaferreira68@hotmail.com>
2024-04-15 22:35:49 -0300

    chore: configurar timezone

--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,6 @@ module ProntuarioVeterinario
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     config.i18n.default_locale = :"pt-BR"
+    config.time_zone = "Brasilia"
   end
 end
