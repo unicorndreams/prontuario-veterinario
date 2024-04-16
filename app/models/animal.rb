628ce95ee52c2e6167fb10f48d772200c7346ebb
Camila Ferreira <camilaferreira68@hotmail.com>
2024-04-16 20:54:24 -0300

    chore: ajustar status

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -15,7 +15,7 @@ class Animal < ApplicationRecord
   before_save -> { self.status = :triagem if status.blank? }
 
   enumerize :genero, in: { macho: 1, femea: 2  }, i18n_scope: "animais_genero", scope: true
-  enumerize :status, in: { triagem: 1, aguardando_soltura: 2, obito: 3 }, i18n_scope: "animais_status", scope: true
+  enumerize :status, in: { triagem: 1, em_reabilitacao: 2, aguardando_soltura: 3, soltura: 4, obito: 5 }, i18n_scope: "animais_status", scope: true
 
   scope :ativos, ->(ativo) { where(ativo: ativo) }
   scope :por_identificador, ->(identificador) { where("identificador ILIKE ?", "%#{identificador}%") if identificador.present? }
